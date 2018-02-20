9a3a3d73c0a33d59ae116cab9793be8e6a75f583
root <root@docker.education.trn>
2018-02-20 00:13:59 -0500

    version 1.0-5
    Added provisioning
    Added UI elements to company tile

--- scripts/company.php
+++ scripts/company.php
@@ -33,5 +33,69 @@
         * @title("Number of queries in MyWeatherDemo UI")
         */
         public $query_counter;
+        
+        /**
+        * @type(string)
+        * @title("Company identifier in MyWeatherDemo")
+        */
+        public $company_id;
+        
+        /**
+        * @type(string)
+        * @title("Login to MyWeatherDemo interface")
+        */
+        public $username;
+        
+        /**
+        * @type(string)
+        * @title("Password for MyWeatherDemo user")
+        */
+        public $password;
+
+        // MyWeatherDemo API URL to work with companies
+        const BASE_URL = "http://www.myweatherdemo.com/api/company/";
+
+        // you can add your own methods as well, don't forget to make them private
+        private function send_curl_request($verb, $url, $payload = ''){
+            $token = "osamwd";
+            $headers = array(
+                    'Content-type: application/json',
+                    'x-provider-token: '. $token
+            );
+            $ch = curl_init();
+            
+            curl_setopt_array($ch, array(
+            CURLOPT_URL            => $url,
+            CURLOPT_RETURNTRANSFER => 1,
+            CURLOPT_CUSTOMREQUEST => $verb,
+            CURLOPT_HTTPHEADER => $headers,
+            CURLOPT_POSTFIELDS => json_encode($payload)
+            ));
+            
+            $response = json_decode(curl_exec($ch));
+            
+            curl_close($ch);
+            return $response;
+        }
+
+        public function provision(){
+            // to create a company in external service we need to pass country, city and name of the company
+            // we can get them from linked core/account resource
+            $request = array(
+                'country' => $this->account->addressPostal->countryName,
+                'city' => $this->account->addressPostal->locality,
+                'name' => $this->account->companyName
+            );
+            $response = $this->send_curl_request('POST', self::BASE_URL, $request);
+            // need to save company_id in APSC, going to use that later to delete a resource in unprovision()
+            // username and password will be used to login to MyWeatherDemo web interface
+            $this->company_id = $response->{'id'};
+            $this->username = $response->{'username'};
+            $this->password = $response->{'password'};
+        }
+
+        public function unprovision(){
+            $this->send_curl_request('DELETE', self::BASE_URL . $this->company_id);
+        }
     }
 ?>
--- ui/company.js
+++ ui/company.js
@@ -5,33 +5,34 @@ define([
     return declare(_View, {
         init: function () {
             return ["aps/Tiles", [
-        ["aps/Tile", {
-                id: "mainid",
-                title: "Company",
-                backgroundColor: "#0077aa",
-                fontColor: "#00ff00",
-                iconName: "../_static/examples/tiles/o365.png",
-                gridSize: "md-8"
-
-            },
-            [
-                ["aps/FieldSet", [
-                    ["aps/Output", {
-                        id: "nameid",
-                        label: "Name",
-                        value: "nanana",
-                        gridSize: "md-8"
-                    }],
-                    ["aps/Output", {
-                        id: "passwordid",
-                        label: "Password",
-                        value: "no data, blya",
-                        gridSize: "md-4"
-                    }]
-                ]]
-            ]
-        ]
-    ]];
-        }
-    });
-});
+                ["aps/Tile", {
+                    id: "mainid",
+                    title: "Company",
+                    backgroundColor: "#0077aa",
+                    fontColor: "#00ff00",
+                    iconName: "../_static/examples/tiles/o365.png",
+                    gridSize: "md-4",
+                    buttons: [{
+                        id: "btnLogin",
+                        title: _('Login'),
+                        iconClass: "fa-external-link",
+                        autoBusy: false,
+                        onClick: function(){
+                          window.open("http://www.myweatherdemo.com/login", "_blank"); }}]},
+                    [["aps/FieldSet", [
+                        ["aps/Output", {
+                            id: "nameid",
+                            label: "Name",
+                            value: "",
+                            gridSize: "md-8" }],
+                        ["aps/Output", {
+                            id: "passwordid",
+                            label: "Password",
+                            value: "",
+                            gridSize: "md-4" }]]]]]]]},
+        onContext: function() {
+            var company = aps.context.vars.company;
+            this.byId("nameid").set("value", company.username);
+            this.byId("passwordid").set("value", company.password);
+            this.byId("mainid").set("title", company.company_id);
+            aps.apsc.hideLoading(); }});});
