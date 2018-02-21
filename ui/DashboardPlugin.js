73ea583180b9e3bab4903bd007f5f8d9c22ce813
root <root@docker.education.trn>
2018-02-21 01:32:24 -0500

    1.0-20 Added tile on home dashboard

---
+++ ui/DashboardPlugin.js
@@ -0,0 +1,7 @@
+define([
+  "dojo/_base/declare",
+  "aps/nav/ViewPlugin"
+], function(declare, ViewPlugin) {
+  return declare(ViewPlugin, {
+    apsType: "http://myweatherdemo.com/company/1.0",
+    entryViewId:  "http://myweatherdemo.com/#company" }); });
